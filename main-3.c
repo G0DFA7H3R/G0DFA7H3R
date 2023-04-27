069e213f9c04e1dbd15c11c7fb24402afcb2761a
Ibrahim <132009001+G0DFA7H3R@users.noreply.github.com>
2023-04-27 15:01:04 -0400

    Update main-3.c
    ConvertDec_to_Hex-Oct-Bin.c is a simple conversion program that prompts the user for their name, followed by a prompt for an integer in the range [1 - 1,000,000] inclusive (or “x” to exit the program). The integer entered will be displayed in decimal, hexadecimal, octal, and binary representation. Hexadecimal, octal, and binary representations must be computed algorithmically (i.e. NOT accomplished via format specifier).

--- main-3.c
+++ main-3.c
@@ -1,14 +1,9 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
+// Ibrahim J
+
 int get_input();
-// Ibrahim Jami, G01296681 , Homework 2
+
 int main()
 {
     int user_input;
